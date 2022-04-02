53de75f3b804f11e6c66695e83352452dde7b8ef
fsilavong <fransilavong@gmail.com>
2022-04-03 00:22:51 +0100

    remove senstry and google analytic

--- label_studio/frontend/src/config/Sentry.ts
+++ label_studio/frontend/src/config/Sentry.ts
@@ -5,22 +5,6 @@ import { Integrations } from "@sentry/tracing";
 import { Route } from 'react-router-dom';
 
 export const initSentry = (history: RouterHistory) => {
-  setTags();
-
-  Sentry.init({
-    dsn: "https://5f51920ff82a4675a495870244869c6b@o227124.ingest.sentry.io/5838868",
-    integrations: [
-      new Integrations.BrowserTracing({
-        routingInstrumentation: ReactSentry.reactRouterV5Instrumentation(history),
-      }),
-    ],
-    environment: process.env.NODE_ENV,
-    // Set tracesSampleRate to 1.0 to capture 100%
-    // of transactions for performance monitoring.
-    // We recommend adjusting this value in production
-    tracesSampleRate: 0.25,
-    release: process.env.RELEASE_NAME,
-  });
 };
 
 const setTags = () => {
